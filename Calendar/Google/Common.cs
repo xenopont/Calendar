ff13e7d29cd0e08735d8dd199ab4dec76f7bc0a9
Sergei Kovalenko <smith404@live.com>
2022-08-23 19:18:13 +0200

    initial commit

---
+++ Calendar/Google/Common.cs
@@ -0,0 +1,23 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Util.Store;
+
+using System.IO;
+using System.Threading;
+
+namespace Calendar.Google
+{
+    internal class Common
+    {
+        static public UserCredential AuthAsync(string credIn, string credOut, string[] scopes)
+        {
+            using var stream = new FileStream(credIn, FileMode.Open, FileAccess.Read);
+
+            return GoogleWebAuthorizationBroker.AuthorizeAsync(
+                GoogleClientSecrets.FromStream(stream).Secrets,
+                scopes,
+                "user",
+                CancellationToken.None,
+                new FileDataStore(credOut, true)).Result;
+        }
+    }
+}
---
+++ Calendar/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Google.Apis.Calendar.v3;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calendar
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Google.Common.AuthAsync("credentials.json", "cred-out.json", new string[] { CalendarService.Scope.CalendarReadonly });
+        }
+    }
+}
